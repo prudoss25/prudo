bf64f944cad76e4b078507eb2562006e59566735
prudoss25 <prudoss25@yahoo.fr>
2019-06-19 04:57:16 -0400

    env access commit modifiee

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token,
+  access_token: ENV["access_token"],
   subscribed_fields: %w[feed mention name]
 )
 Bot.on :message do |message|
